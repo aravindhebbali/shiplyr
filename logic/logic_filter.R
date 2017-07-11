196f765173119b2152c97ce6d5f2a317b94b18ca
rsquaredin <rsquaredin@gmail.com>
2017-07-11 15:58:18 +0530

    data filtering prototype

--- logic/logic_filter.R
+++ logic/logic_filter.R
@@ -7,10 +7,24 @@ observeEvent(input$submit_dply_selvar, {
   )
 })
 
-filtered <- eventReactive(input$submit_dply_filt, {
-  k <-final_sel() %>%
-    filter_(paste(input$dplyr_filter, input$dplyr_filt_op, input$dplyr_filt_val))
-  k
-  # k <- filter_(final_sel(), paste(input$dplyr_filter, input$dplyr_filt_op, input$dplyr_filt_val))
-  # k
+filt_data <- reactiveValues(p = NULL)
+
+observeEvent(input$submit_dply_selvar, {
+  filt_data$p <- final_sel()
 })
+
+observeEvent(input$submit_dply_filt, {
+
+  filt_data$p <- filt_data$p %>%
+    filter_(paste(input$dplyr_filter, input$dplyr_filt_op, input$dplyr_filt_val))
+
+})
+
+
+# filtered <- eventReactive(input$submit_dply_filt, {
+#   k <- final_sel() %>%
+#     filter_(paste(input$dplyr_filter, input$dplyr_filt_op, input$dplyr_filt_val))
+#   k
+#   # k <- filter_(final_sel(), paste(input$dplyr_filter, input$dplyr_filt_op, input$dplyr_filt_val))
+#   # k
+# })
--- ui/ui_filter.R
+++ ui/ui_filter.R
@@ -4,24 +4,30 @@ tabPanel('Filter', value = 'tab_filter',
 
 		fluidRow(
 
-			selectInput(
-				inputId = 'dplyr_filter',
-				label = 'Filter:',
-				choices = '',
-				selected = ''
+			column(12, align = 'center',
+				selectInput(
+					inputId = 'dplyr_filter',
+					label = 'Filter:',
+					choices = '',
+					selected = ''
+				)
 			),
 
-			selectInput(
-				inputId = 'dplyr_filt_op',
-				label = 'Select Operator',
-				choices = c('<', '>', '<=', '>=', '=='),
-				selected = ''
+			column(12, align = 'center',
+				selectInput(
+					inputId = 'dplyr_filt_op',
+					label = 'Select Operator',
+					choices = c('<', '>', '<=', '>=', '=='),
+					selected = ''
+				)
 			),
 
-			textInput(
-				inputId = 'dplyr_filt_val',
-				label = 'Value',
-				value = '20'
+			column(12, align = 'center',
+				textInput(
+					inputId = 'dplyr_filt_val',
+					label = 'Value',
+					value = '20'
+				)
 			)
 
 		),
@@ -31,7 +37,7 @@ tabPanel('Filter', value = 'tab_filter',
 			column(12, align = 'center',
         br(),
         br(),
-        actionButton(inputId = 'submit_dply_filt', label = 'Submit', width = '120px', icon = icon('check')),
+        actionButton(inputId = 'submit_dply_filt', label = 'Filter', width = '120px', icon = icon('check')),
         bsTooltip("submit_dply_filt", "Click here to filter data.",
                       "bottom", options = list(container = "body"))
       )
