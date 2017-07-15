2dc483e6b3d5a9a58d960362be25df927b08ac3b
rsquaredin <rsquaredin@gmail.com>
2017-07-15 13:41:41 +0530

    add links to view tab

--- logic/logic_view.R
+++ logic/logic_view.R
@@ -1,3 +1,8 @@
 output$table <- renderDataTable({
  	final_sample$d
 })
+
+observeEvent(input$view2getdata, {
+	updateNavbarPage(session, 'mainpage', selected = 'tab_upload')
+  updateNavlistPanel(session, 'navlist_up', 'tab_datasources')
+})
