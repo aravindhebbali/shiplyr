36d51e780799ff738cba18877ea366d59624e46d
rsquaredin <rsquaredin@gmail.com>
2017-07-31 22:47:13 +0530

    integration test for histogram

--- server.R
+++ server.R
@@ -82,6 +82,7 @@ shinyServer(function(input, output, session) {
     source('logic/logic_gbar2.R', local = T)
     source('logic/logic_gbox.R', local = T)
     source('logic/logic_gbox2.R', local = T)
+    source('logic/logic_ghist.R', local = T)
 
 })
 
