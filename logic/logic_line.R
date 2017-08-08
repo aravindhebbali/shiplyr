d37c67787768fc362956ee65e60b330631f5ece8
rsquaredin <rsquaredin@gmail.com>
2017-08-08 16:31:16 +0530

    allow date variables in X axis

--- logic/logic_line.R
+++ logic/logic_line.R
@@ -7,12 +7,31 @@ source("helper/line-plot.R")
     # })
 
     observeEvent(input$button_split_no, {
-        num_data <- final_split$train[, sapply(final_split$train, is.numeric)]
+      num_data <- cbind.data.frame(final_split$train[, sapply(final_split$train, is.numeric)],
+        final_split$train[, sapply(final_split$train, is.Date)])
+    
+      k <- final_split$train %>%
+        map(is.numeric) %>%
+        unlist()
+
+      t <- final_split$train %>%
+        map(is.Date) %>%
+        unlist()
+
+      j1 <- names(which(k == TRUE))
+      j2 <- names(which(t == TRUE))
+
+      if (length(j1) == 0) {
+        j <- j2
+      } else if (length(j2) == 0) {
+        j <- j1
+      } else {
+        j <- c(j1, j2)
+      }
+      colnames(num_data) <- j
+      
         if (is.null(dim(num_data))) {
-            k <- final_split$train %>% map(is.numeric) %>% unlist()
-            j <- names(which(k == TRUE))
             numdata <- tibble::as_data_frame(num_data)
-            colnames(numdata) <- j
             updateSelectInput(session, 'line_select_x',
               choices = names(numdata), selected = names(numdata))
             updateSelectInput(session, 'line_select_y',
@@ -29,12 +48,31 @@ source("helper/line-plot.R")
     })
 
     observeEvent(input$submit_part_train_per, {
-        num_data <- final_split$train[, sapply(final_split$train, is.numeric)]
+        num_data <- cbind.data.frame(final_split$train[, sapply(final_split$train, is.numeric)],
+        final_split$train[, sapply(final_split$train, is.Date)])
+    
+      k <- final_split$train %>%
+        map(is.numeric) %>%
+        unlist()
+
+      t <- final_split$train %>%
+        map(is.Date) %>%
+        unlist()
+
+      j1 <- names(which(k == TRUE))
+      j2 <- names(which(t == TRUE))
+
+      if (length(j1) == 0) {
+        j <- j2
+      } else if (length(j2) == 0) {
+        j <- j1
+      } else {
+        j <- c(j1, j2)
+      }
+      colnames(num_data) <- j
+      
         if (is.null(dim(num_data))) {
-            k <- final_split$train %>% map(is.numeric) %>% unlist()
-            j <- names(which(k == TRUE))
             numdata <- tibble::as_data_frame(num_data)
-            colnames(numdata) <- j
             updateSelectInput(session, 'line_select_x',
               choices = names(numdata), selected = names(numdata))
             updateSelectInput(session, 'line_select_y',
