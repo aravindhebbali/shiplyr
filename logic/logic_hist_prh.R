033d6b0206a4b8d5fd8d3f02334a0ea4c09c20ac
rsquaredin <rsquaredin@gmail.com>
2017-08-08 20:02:03 +0530

    additional plotly histogram options

--- logic/logic_hist_prh.R
+++ logic/logic_hist_prh.R
@@ -54,10 +54,31 @@ observeEvent(input$submit_part_train_per, {
 })
 
 
+histlydata <- reactive({
+  req(input$histly_select_x)
+  final_split$train[, input$histly_select_x]
+})
+
+observe({
+  updateNumericInput(session, "histly_binstart", value = min(histlydata()))
+  updateNumericInput(session, "histly_binend", value = max(histlydata()))
+})
+
 output$histly_plot_1 <- renderPlotly({
-  histly(data = final_split$train, y = input$histly_select_x, 
-    title = input$histly_title, x_title = input$histly_xlabel,
-    y_title = input$histly_ylabel)
+  if (input$histly_auto == TRUE) {  
+      histly(data = final_split$train, y = input$histly_select_x, 
+        title = input$histly_title, x_title = input$histly_xlabel, hist_col = input$histly_color,
+        y_title = input$histly_ylabel, hist_orient = input$histly_horiz, 
+        hist_opacity = input$histly_opacity, hist_type = input$histly_type, 
+        auto_binx = input$histly_auto)
+  } else {
+    histly(data = final_split$train, y = input$histly_select_x, 
+        title = input$histly_title, x_title = input$histly_xlabel, hist_col = input$histly_color,
+        y_title = input$histly_ylabel, hist_orient = input$histly_horiz, 
+        hist_opacity = input$histly_opacity, hist_type = input$histly_type, 
+        auto_binx = input$histly_auto, xbins_size = input$histly_binsize,
+        xbins_start = input$histly_binstart, xbins_end = input$histly_binend)
+  }
 })
 
 output$bohist_plot_1 <- renderRbokeh({
